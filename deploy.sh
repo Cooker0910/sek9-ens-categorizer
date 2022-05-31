350eb26a18477c9ceeceaad17a3d4ff0aa4705c2
dev0088 <ninjadev999@gmail.com>
2022-05-31 10:17:14 -0400

    Updated deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 
-docker stop sek9_ens_app
+docker stop sek9_ens_categorizer
 
 yes | docker system prune
 
 #git pull origin main
 
-docker build -t sek9_ens_app . -f Dockerfile
+docker build -t sek9_ens_categorizer . -f Dockerfile
 
 docker run \
-  --name sek9_ens_app \
+  --name sek9_ens_categorizer \
   --network sek9 \
   --ip 172.20.0.5 \
-  --hostname sek9_ens_app \
+  --hostname sek9_ens_categorizer \
   --dns=8.8.8.8 \
   -v ${PWD}:/app \
   -v /app/node_modules \
   -p 3000:3000 \
   -e CHOKIDAR_USEPOLLING=true \
-  -d sek9_ens_app
+  -d sek9_ens_categorizer
 
 yes | docker system prune
